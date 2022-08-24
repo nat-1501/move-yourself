fda2f357fcd5b90a9c023e4f17d7b28663ee8f73
Nat-1501 <natalisoares90@gmail.com>
2022-08-24 14:17:02 -0300

    Transmitindo para next

---
+++ move-yourself-next/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { ExperienceBar } from "./components/ExperienceBar";
+import './styles/global.css';
+
+function App() {
+  return (
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"></link>
+    </Head>
+  );
+
+}
