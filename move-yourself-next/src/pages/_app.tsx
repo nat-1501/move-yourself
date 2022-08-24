0309b854c89a1c7cf4b13afbb511f3432ca41b58
Nat-1501 <natalisoares90@gmail.com>
2022-08-24 15:15:07 -0300

    alterando experience bar

--- move-yourself-next/src/pages/_app.tsx
+++ move-yourself-next/src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Component {...pageProps} />
+  )
 }
-
-export default MyApp
--- move-yourself-next/src/pages/index.tsx
+++ move-yourself-next/src/pages/index.tsx
@@ -1,14 +1,10 @@
-import Head from 'next/head'
-import { ExperienceBar } from "./components/ExperienceBar";
-import './styles/global.css';
+import { ExperienceBar } from "../components/ExperienceBar";
 
-function App() {
+export default function Home() {
   return (
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"></link>
-    </Head>
-  );
+   <div className="container">
+    <ExperienceBar />
+   </div>
+  )
 
 }
