e4163118b9a71de5d2e356425adde913fba23317
Nat-1501 <natalisoares90@gmail.com>
2022-08-24 15:46:08 -0300

    Criando componente sessão

---
+++ move-yourself-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/nat-1501.png" alt="Nat Soares" />
+            <div>
+                <strong>Natali Soares</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
