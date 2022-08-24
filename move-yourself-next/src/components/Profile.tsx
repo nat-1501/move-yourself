65bb37a67c2b39fe365d3dbf8e970fb5ddb0b6be
Nat-1501 <natalisoares90@gmail.com>
2022-08-24 17:30:28 -0300

    Concluido componente perfil

--- move-yourself-next/src/components/Profile.tsx
+++ move-yourself-next/src/components/Profile.tsx
@@ -1,10 +1,15 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
     return (
-        <div>
+        <div className={styles.profileContainer}>
             <img src="https://github.com/nat-1501.png" alt="Nat Soares" />
             <div>
                 <strong>Natali Soares</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
             </div>
         </div>
     );
