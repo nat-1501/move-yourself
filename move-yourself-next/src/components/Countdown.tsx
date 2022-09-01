76c1962f830f6545cb9b4b32bdc0a96363105da9
Nat-1501 <natalisoares90@gmail.com>
2022-09-01 11:59:25 -0300

    utilizando fragmentos

--- move-yourself-next/src/components/Countdown.tsx
+++ move-yourself-next/src/components/Countdown.tsx
@@ -6,6 +6,7 @@ let cowntdownTimeout: NodeJS.Timeout;
 export function Countdown() {
     const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60); 
     const seconds = time % 60;
@@ -28,6 +29,9 @@ export function Countdown() {
                 cowntdownTimeout = setTimeout(() => {
                    setTime( time -1); 
                 }, 1000);
+            } else if (isActive && time == 0) {
+                setHasFinished(true);
+                setIsActive(false);
             }
         }, [isActive, time])
 
@@ -45,6 +49,17 @@ export function Countdown() {
                 </div>
             </div>
 
+    {hasFinished ? (
+        <button
+            disabled
+            className={styles.countdownutton}
+        >
+            Ciclo encerrado
+        </button>
+    ) : (
+
+        <>
+            
         { isActive ? (    
             <button 
                 type="button" 
@@ -61,7 +76,10 @@ export function Countdown() {
             >
                 Iniciar um ciclo
             </button> 
-            )}    
+         )}    
+        </>
+          
+        )}
         </div>    
     );
 }
