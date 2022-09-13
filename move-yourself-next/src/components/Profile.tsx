115d0d53ee96e5478ef9f653120e2ac1baeffd11
Nat-1501 <natalisoares90@gmail.com>
2022-09-12 22:40:33 -0300

    desafio concluido cada vez que e feito o desafio

--- move-yourself-next/src/components/Profile.tsx
+++ move-yourself-next/src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/nat-1501.png" alt="Nat Soares" />
@@ -8,7 +11,7 @@ export function Profile() {
                 <strong>Natali Soares</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level { level }
                 </p>
             </div>
         </div>
