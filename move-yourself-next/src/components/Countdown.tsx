e13261bd6cc82a238ef2054bad3239abc60c97ea
Nat-1501 <natalisoares90@gmail.com>
2022-09-14 16:33:28 -0300

    icializando countdown assim que clica em falhei ou completei

--- move-yourself-next/src/components/Countdown.tsx
+++ move-yourself-next/src/components/Countdown.tsx
@@ -49,7 +49,7 @@ export function Countdown() {
             <button 
                 type="button" 
                 className={'styles.countdownButton} ${styles.countdownButtonActive}'}
-                onClick={resetCowntdown}
+                onClick={resetCountdown}
             >
              Abandonar ciclo
             </button>  
--- move-yourself-next/src/pages/index.tsx
+++ move-yourself-next/src/pages/index.tsx
@@ -7,6 +7,7 @@ import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
     </Head>
     <ExperienceBar />
 
+
+    <CountdownProvider>
       <section>
         <div>
           <Profile />
@@ -27,6 +30,7 @@ export default function Home() {
           
         </div>
       </section>
+    </CountdownProvider>  
    </div>
 
   )
